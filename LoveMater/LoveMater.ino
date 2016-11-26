87eca9c4845edfcadee9e804689ce0093311087e
roadman <roadmang@gmail.com>
2016-11-26 15:13:01 +0900

    add arguino program LoveMater,SpaceShipController

---
+++ LoveMater/LoveMater.ino
@@ -0,0 +1,46 @@
+const int sensorPin = A0;
+const float baselineTemp = 20.0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  for(int pinNumber = 2; pinNumber < 5; pinNumber++) {
+    pinMode(pinNumber, OUTPUT);
+    digitalWrite(pinNumber, LOW);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sensorVal = analogRead(sensorPin);
+
+  Serial.print("Sensor Value: ");
+  Serial.print(sensorVal);
+
+  // ADコンバータから読み取った値を電圧に変換する
+  float voltage = (sensorVal/1024.0) * 5.0;
+  Serial.print(", Volts: ");
+  Serial.print(voltage);
+  Serial.print(", degrees C: ");
+  // 電圧を温度に変換する
+  float temperature = (voltage - .5) * 100;
+  Serial.println(temperature);
+  if(temperature < baselineTemp) {
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+  } else if(temperature >= baselineTemp+2 && temperature < baselineTemp+2) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+  } else if(temperature >= baselineTemp+2 && temperature < baselineTemp+4) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+  } else if(temperature >= baselineTemp+2 && temperature < baselineTemp+6) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+  }
+  delay(1);
+}
